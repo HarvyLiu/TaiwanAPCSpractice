0f647f21806b9e8f64ccd85ef05efff1c0084c85
Harvy <harvyliu2009@gmail.com>
2025-11-12 00:43:48 +0800

    Update a010.py

--- ZeroJudge/a010.py
+++ ZeroJudge/a010.py
@@ -1,22 +1,25 @@
 num = int(input())
-i = 1
-list = []
-k = 0
-string1 = ""
-string2 = ""
-while True:
-    if num > 1:
-        i += 1
-        if num % i == 0:
-            num /= i
-            list.append(i)
-            i = 0
-        else:
-            continue
-    elif num == 1:
-        break
-for j in list:
-    for list[j] in list:
-        k += 1
-        string2 = f"{list[j]}^k"
-    string1 += string2
+original = num
+factors = {}
+i = 2
+
+# prime factorization
+while i * i <= num:
+    while num % i == 0:
+        num //= i
+        factors[i] = factors.get(i, 0) + 1
+    i += 1
+
+if num > 1:
+    factors[num] = factors.get(num, 0) + 1
+
+# formatting
+parts = []
+for prime, exp in factors.items():
+    if exp == 1:
+        parts.append(f"{prime}")
+    else:
+        parts.append(f"{prime}^{exp}")
+
+print(f"{' * '.join(parts)}")
+
