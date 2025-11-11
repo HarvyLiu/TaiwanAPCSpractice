2efea58a5270e22b9b1f386418ee1ad414e2449f
劉鎬維 <harvyliu2009@gmail.com>
2025-11-11 15:13:35 +0800

    Update a006.py

--- ZeroJudge/a006.py
+++ ZeroJudge/a006.py
@@ -1,9 +1,11 @@
+import math
 a, b, c = map(int, input().split())
-o = (-b+(b**2-4*a*c)**0.5)/2*a
-p = (-b-(b**2-4*a*c)**0.5)/2*a
+
+o = (-b+(b**2-4*a*c)**0.5)/(2*a)
+p = (-b-(b**2-4*a*c)**0.5)/(2*a)
 if b**2-4*a*c < 0:
     print("No real root")
 elif b**2-4*a*c == 0:
-    print(f"Two same roots {o}")
+    print(f"Two same roots x={math.floor(o)}")
 elif b**2-4*a*c > 0:
-    print(f"Two different roots x1={o} , x2={p}")
+    print(f"Two different roots x1={round(o)} , x2={round(p)}")
